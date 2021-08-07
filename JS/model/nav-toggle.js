6d38a38179cba4174aa3737f0e484877f2e29c2c
Cam93 <camille.nicolas93@gmail.com>
2021-08-07 16:34:45 +0200

    change header

--- JS/model/nav-toggle.js
+++ JS/model/nav-toggle.js
@@ -1,20 +1,9 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+$(function () {
+  $(window).on('scroll', function () {
+      if ( $(window).scrollTop() > 10 ) {
+          $('.navbar').addClass('active');
+      } else {
+          $('.navbar').removeClass('active');
       }
-    }
-  }
-}
+  });
+});
